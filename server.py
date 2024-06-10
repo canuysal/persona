e5b25e245b9e3adc046b9ccd996a505480bfdfbc
Can Uysal <canuysal.1@gmail.com>
2024-06-11 02:56:55 +0300

    Add arguement parser.

--- server.py
+++ server.py
@@ -8,73 +8,90 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langserve import add_routes
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-
+import argparse
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4o")
+def runServer():
+    model = ChatOpenAI(model="gpt-4o")
 
-loader = DirectoryLoader("./data")
-docs = loader.load()
+    loader = DirectoryLoader(dataDir)
+    docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-retriever = vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever()
 
-userName = os.environ.get("USER_NAME")
+    systemPrompt = (
+        "You are 'Persona', {userName}'s assistant. "
+        "You only answer questions related to {userName}."
+        "Use the following context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
 
-system_prompt = (
-    "You are 'Persona', {userName}'s assistant. "
-    "You only answer questions related to {userName}."
-    "Use the following context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
-)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", systemPrompt),
+            ("human", "{input}"),
+        ]
+    )
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+    rag_chain = (
+        {"context": retriever, "userName": lambda x: os.environ.get("USER_NAME"), "input": RunnablePassthrough()}
+        | prompt
+        | model
+        | StrOutputParser()
+    )
 
-rag_chain = (
-    {"context": retriever, "userName": lambda x: os.environ.get("USER_NAME"), "input": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
+    app = FastAPI(
+        title=f"{os.environ.get('USER_NAME')}'s Q&A Server",
+        version="1.0",
+        description="Simple Q&A server with a rag chain.",
+    )
 
-app = FastAPI(
-    title=f"{os.environ.get('USER_NAME')}'s Q&A Server",
-    version="1.0",
-    description="Simple Q&A server with a rag chain.",
-)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["*"],
+    )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-)
-
-# Adds routes to the app for using the chain under:
-# /invoke
-# /batch
-# /stream
-add_routes(app, rag_chain, path="/chat")
+    # Adds routes to the app for using the chain under:
+    # /invoke
+    # /batch
+    # /stream
+    add_routes(app, rag_chain, path="/chat")
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
-    import uvicorn
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=7300,
+        help='An integer for the port number'
+    )
+    parser.add_argument(
+        '--datadir',
+        type=str,
+        default="./data",
+        help='A string for the data directory'
+    )
 
-    uvicorn.run(app, host="0.0.0.0", port=7300)
+    args = parser.parse_args()
+
+    port = args.port
+    dataDir = args.datadir
+    runServer()
